da757ba20f6d6806a55bda3eb11b3ad42c258519
nikkihaleyowo <opnamanama@gmail.com>
2024-10-21 02:28:28 -0500

    initial commit

--- src/components/LoadUser.jsx
+++ src/components/LoadUser.jsx
@@ -13,7 +13,7 @@ const LoadUser = (props) => {
   useEffect( async ()=>{
     console.log("sss: "+props.user.email)
     let exists = false;
-    await axios.get(`/api/users/find/${props.user.email}`)
+    await axios.get(`https://policy-backend-nafh.onrender.com/api/users/find/${props.user.email}`)
     .then(result => {
       if(result.status === 200){
         exists = true;
@@ -29,7 +29,7 @@ const LoadUser = (props) => {
     .catch(err=>console.log(err))
     if(!exists){
       console.log(props.user.email)
-      axios.post(`/api/users/create`, {email: props.user.email})
+      axios.post(`https://policy-backend-nafh.onrender.com/api/users/create`, {email: props.user.email})
         .then(result => {
           console.log('making a new user')
           const data = result.data;
